44421a5e7195e45b1c6f513d9e0f94525be8cfde
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-12-23 03:55:15 +0800

    Add Clerk authentication and middleware

--- src/app/_components/auth.tsx
+++ src/app/_components/auth.tsx
@@ -1,16 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 
 export function SignUp() {
+  const router = useRouter();
+
   return (
-    <Button variant={"default"} className="text-sm font-semibold">
+    <Button
+      onClick={() => router.push("/auth/sign-up")}
+      variant={"default"}
+      className="text-sm font-semibold"
+    >
       Sign up
     </Button>
   );
 }
 
 export function SignIn() {
+  const router = useRouter();
+
   return (
-    <Button variant={"ghost"} className="text-sm font-semibold">
+    <Button
+      onClick={() => router.push("/auth/sign-in")}
+      variant={"ghost"}
+      className="text-sm font-semibold"
+    >
       Sign in
     </Button>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -10,7 +11,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
-      {children}
+      <ClerkProvider>{children}</ClerkProvider>
     </ThemeProvider>
   );
 }
